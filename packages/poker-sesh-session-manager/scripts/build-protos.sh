3ae671428c93b92a8b1b53a8afed497fb797e244
CRasico <53454804+CRasico@users.noreply.github.com>
2022-02-25 00:01:10 -0500

    Health Check (#3)
    * updates
    
    * getting closer :)
    
    * that's sick we build the GRPC health check
    
    * add some comments around what and why I was doing these things
    
    * more self review

--- packages/poker-sesh-session-manager/scripts/build-protos.sh
+++ packages/poker-sesh-session-manager/scripts/build-protos.sh
@@ -2,21 +2,23 @@
 
 BASEDIR=$(dirname "$0")
 GEN_PROTOCOL_BUFFER_DEST=./src/protocol-buffers
+BUILD_PROTOCOL_BUFFER_DEST=./dist/protocol-buffers
 
 cd ${BASEDIR}/../
-mkdir -p ${PROTO_DEST}
+mkdir -p ${GEN_PROTOCOL_BUFFER_DEST}
 
-# JavaScript code generation
-yarn run grpc_tools_node_protoc \
-    --js_out=import_style=commonjs,binary:${GEN_PROTOCOL_BUFFER_DEST} \
-    --grpc_out=${GEN_PROTOCOL_BUFFER_DEST} \
-    --plugin=protoc-gen-grpc=./node_modules/.bin/grpc_tools_node_protoc_plugin \
-    -I ./protocol-buffers \
-    protocol-buffers/*.proto
-
-# TypeScript code generation
+# Typescript and Javascript code generation
 yarn run grpc_tools_node_protoc \
     --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
-    --ts_out=${GEN_PROTOCOL_BUFFER_DEST} \
+    --ts_out=grpc_js:${GEN_PROTOCOL_BUFFER_DEST} \
+    --js_out=import_style=commonjs,binary:${GEN_PROTOCOL_BUFFER_DEST} \
+    --grpc_out=grpc_js:${GEN_PROTOCOL_BUFFER_DEST} \
     -I ./protocol-buffers \
     protocol-buffers/*.proto
+
+# Copy Build JS files to folder, as they shouldn't be retranspiled.
+mkdir ${BUILD_PROTOCOL_BUFFER_DEST}
+for file in ${GEN_PROTOCOL_BUFFER_DEST}/*.js
+do
+    cp $file ${BUILD_PROTOCOL_BUFFER_DEST}/
+done  
--- packages/poker-sesh-session-manager/src/index.ts
+++ packages/poker-sesh-session-manager/src/index.ts
@@ -1,4 +1,21 @@
-import { Server } from "grpc";
-import { IHelloServiceServer } from "./protocol-buffers/hello_grpc_pb";
+import { Server, ServerCredentials } from "@grpc/grpc-js";
+import { HealthService } from "./protocol-buffers/health_grpc_pb";
+import { HealthServer } from "./server/health";
 
-const server = new Server();
+const port: string | number = process.env.port || 50051;
+const server: Server = new Server();
+
+// @ts-ignore
+server.addService(HealthService, new HealthServer());
+
+server.bindAsync(
+	`localhost:${port}`, 
+	ServerCredentials.createInsecure(),
+	(error: Error | null, port: number) => {
+		if (error) {
+			console.error('an issue occured starting up the server {error}', error);
+		}
+		console.log('started server {port}', port);
+		server.start();
+	} 
+);
